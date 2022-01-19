4afce85ecb5322fb7fce8f853f0b26c8f5ac7aaf
Yich <yich.lin@xuenn.com>
2022-01-19 14:35:10 +0800

    Site updated: 2022-01-19 14:35:06 +08:00

---
+++ deploy.ps1
@@ -0,0 +1,36 @@
+# Build the project.
+
+hugo
+
+# Go to public directory.
+
+Set-Location .\public
+
+# Add changes to git.
+
+git add .
+
+# Create a commit message.
+
+$dateTime = Get-Date -format "yyyy-MM-dd HH:mm:ss K"
+$commitMsg = "Site updated: ${dateTime}"
+
+# Commit.
+
+git commit -m $commitMsg
+
+# Push source and build repos.
+
+git push origin master
+
+# Come back to the project root.
+
+Set-Location .\..
+
+git add .
+
+git commit -m $commitMsg
+
+git push origin master
+
+Write-Host "The web page has been updated!" -ForegroundColor "Cyan"
